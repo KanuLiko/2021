5edf9bc8ad1ed6763d7630219fc3585b486e6b17
KanuLiko <13135540882@163.com>
2021-01-30 23:34:31 +0800

    小改动

--- C_Code/main.c
+++ C_Code/main.c
@@ -1,32 +1,36 @@
-#include<stdarg.h>
+#include <stdarg.h>
 #include "itoa.h"
 #include <stdio.h>
-#define write32(v,a) (*(volatile unsigned int *)(a) = (v))
-#define _INTSIZEOF(n) ((sizeof(n)+sizeof(int)-1)&(sizeof(int)-1))
-void print_bin(int n){
-    int l = sizeof(n)*8;
+#define write32(v, a) (*(volatile unsigned int *)(a) = (v))
+#define _INTSIZEOF(n) ((sizeof(n) + sizeof(int) - 1) & (sizeof(int) - 1))
+void print_bin(int n)
+{
+    int l = sizeof(n) * 8;
     int i;
-    if(i==0){
+    if (i == 0)
+    {
         printf("0");
         return;
     }
-    for(i=l-1;i>=0;i--){
-        if(n&(1<<i))
+    for (i = l - 1; i >= 0; i--)
+    {
+        if (n & (1 << i))
             break;
     }
-    for(;i>=0;i--)
-        printf("%d",(n&(1<<i))!=0);
+    for (; i >= 0; i--)
+        printf("%d", (n & (1 << i)) != 0);
 }
-int main(int args,const char** argv){
+int main(int args, const char **argv)
+{
     // char *a = NULL;
     // char str[32];
     // printf("%d",_INTSIZEOF(a));
-    int a =13;
-    int b =21;
+    int a = 13;
+    int b = 21;
     char *buf1;
     char *buf2;
     char *str;
-    printf("%s\n",itoa(buf1,a));
+    printf("%s\n", itoa(buf1, a));
     /*
     printf("%s\n", itoa(1u<<a,str,2));
     printf("%s\n",itoa(24,str,2));
--- C_Code/tempCodeRunnerFile.c
+++ C_Code/tempCodeRunnerFile.c
@@ -1,21 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
-void print_bin(int n){
-    int l = sizeof(n)*8;
+#include <stdio.h>
+#include <stdlib.h>
+void print_bin(int n)
+{
+    int l = sizeof(n) * 8;
     int i;
-    if(i==0){
+    if (i == 0)
+    {
         printf("0");
         return;
     }
-    for(i=l-1;i>=0;i--){
-        if(n&(1<<i))
+    for (i = l - 1; i >= 0; i--)
+    {
+        if (n & (1 << i))
             break;
     }
-    for(;i>=0;i--)
-        printf("%d",(n&(1<<i))!=0);
+    for (; i >= 0; i--)
+        printf("%d", (n & (1 << i)) != 0);
 }
-int main(int args,const char** argv){
+int main(int args, const char **argv)
+{
     int a = 8;
-    print_bin(a);
+    printf("the answer is %d",a);
     return 0;
 }
