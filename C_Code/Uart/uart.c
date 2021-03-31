d39bf95525d2112639b85c8d0c02422f964aba0a
KanuLiko <13135540882@163.com>
2021-03-31 13:20:18 +0800

    remove some files

--- C_Code/Uart/uart.c
+++ C_Code/Uart/uart.c
@@ -4,12 +4,15 @@ static volatile unsigned int *IOMUXC_SW_MUX_CTL_PAD_UART1_TX_DATA;
 static volatile unsigned int *IOMUXC_SW_MUX_CTL_PAD_UART1_RX_DATA;
 
 void Uart_Init(void){
+    //由于时钟的默认情形已经是满足要求了,所以这里没有设置相关的寄存器
     IOMUXC_SW_MUX_CTL_PAD_UART1_TX_DATA = (volatile unsigned int *)0x20E0084;
     IOMUXC_SW_MUX_CTL_PAD_UART1_RX_DATA = (volatile unsigned int *)0x20E0088;
 
+    //设置脚位复用寄存器的功能为uart1
     *IOMUXC_SW_MUX_CTL_PAD_UART1_RX_DATA = 0;
     *IOMUXC_SW_MUX_CTL_PAD_UART1_TX_DATA = 0;
 
+    //
     UART1->UCR1 |= (1 << 0);
 
     //设置uart的传输格式:
