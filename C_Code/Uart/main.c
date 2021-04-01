86264ec5a7393165cf52902243350eb778b9201d
KanuLiko <13135540882@163.com>
2021-04-01 15:04:47 +0800

    4-1-15-04

--- C_Code/Uart/main.c
+++ C_Code/Uart/main.c
@@ -1,9 +1,12 @@
 #include "uart.h"
 int main(){
     unsigned char cTestData = 'F';
+    unsigned char cTestDataSpace = 36;
     Uart_Init();
     while(1){
         PutChar(cTestData);
+        PutChar(cTestDataSpace);
+        for(int i=0;i<100;i++);
     }
     return 0;
 }
