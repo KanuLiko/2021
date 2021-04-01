571c4c16a458e85c4b15fb21704e11df983f4723
KanuLiko <13135540882@163.com>
2021-04-01 14:41:37 +0800

    experiment

--- C_Code/Uart/main.c
+++ C_Code/Uart/main.c
@@ -1,11 +1,16 @@
 #include "uart.h"
-
+void delay(void){
+    unsigned int i;
+    for(i=0;i<1000;i++);
+}
 int main(){
-    unsigned char cTestData = 'A';
+    unsigned char cTestData;
+    int i;
     Uart_Init();
-    while (1)
-    {
+    for(i=0;i<10;i++){
+        cTestData = (char)i;
         PutChar(cTestData);
+        delay();
     }
     return 0;
 }
