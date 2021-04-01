3bec93bdcb474c5a9844914cd4617f1021727c2a
KanuLiko <13135540882@163.com>
2021-04-01 14:58:41 +0800

    4/1/14-58

--- C_Code/Uart/main.c
+++ C_Code/Uart/main.c
@@ -1,14 +1,9 @@
 #include "uart.h"
-void delay(void){
-    unsigned int i;
-    for(i=0;i<1000;i++);
-}
 int main(){
     unsigned char cTestData = 'F';
     Uart_Init();
     while(1){
         PutChar(cTestData);
-        delay();
     }
     return 0;
 }
